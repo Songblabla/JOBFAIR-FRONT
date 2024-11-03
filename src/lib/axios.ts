8cdaefb94e4770599faefb481e1079a3b16eefd0
HEKYPTO <tsunyanapat@gmail.com>
2024-11-03 23:14:07 +0700

    DEBUG

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
+console.log("Backend URL", process.env.NEXT_PUBLIC_BACKEND_URL);
+
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,  
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
-// console.log("Backend URL", process.env.NEXT_PUBLIC_BACKEND_URL);
 
 api.interceptors.request.use((config) => {
   // console.log(config);
