0ebe39cdf38c43b50981a53e4ccbfdfd4a21e3ab
HEKYPTO <tsunyanapat@gmail.com>
2024-11-14 16:58:09 +0700

    ADD HEADER

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,6 +5,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,  
   headers: {
+    'Accept': 'application/json',
     'Content-Type': 'application/json',
   },
 });
