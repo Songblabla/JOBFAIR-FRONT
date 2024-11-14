97abed3a01dcc5b02bee30ee28eed74306f62168
HEKYPTO <tsunyanapat@gmail.com>
2024-11-15 00:52:14 +0700

    EMBED BASE

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: "https://jobfairbackend.tsunyanapat.com/api/v1",
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
   },
 });
+
+// console.log(api);
+
+// console.log("hello");
+
+// axios.get(`https://jobfairbackend.tsunyanapat.com/api/v1/me`)
+//   .then(response => console.log(response))
+//   .catch(error => console.error(error));
 
 api.interceptors.request.use(
   (config) => {
