d2d1c8ec375f086fe4455fd85399976498b202c5
HEKYPTO <tsunyanapat@gmail.com>
2024-11-14 21:47:05 +0700

    HUH

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,24 +1,24 @@
 import axios from 'axios';
 
-// console.log("Backend URL", process.env.NEXT_PUBLIC_BACKEND_URL);
-
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,  
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
   },
 });
 
-api.interceptors.request.use((config) => {
-  // console.log(config);
-  const token = localStorage.getItem('token'); 
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); 
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default api;
